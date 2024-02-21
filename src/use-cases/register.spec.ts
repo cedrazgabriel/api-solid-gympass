c61dc728bfd220a12ca2d10b04c90b466ac08ba3
Gabriel Cedraz <cedrazdev@gmail.com>
2024-02-20 22:04:54 -0300

    test: create unit test of hashed password is correct

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,37 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('testando se tá funcionando rsrs', () => {
-  expect(2 + 2).toBe(4)
+describe('register use case testes', () => {
+  it('a senha do usuário deve ser hasheada no cadastro', async () => {
+    const registerUseCase = new RegisterUseCase({
+      // eslint-disable-next-line
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'Fulano',
+      email: 'fulano@fulano.com.br',
+      password: 'senhaqualquer1234',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      'senhaqualquer1234',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })

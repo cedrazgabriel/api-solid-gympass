b2b09382e67bb5c7e73c73ee4342ce74b072b68a
Gabriel Cedraz <cedrazdev@gmail.com>
2024-03-02 19:40:09 -0300

    test: create e2e tests to register controller

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,43 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('teste funcionamento', () => {
-  expect(1).toEqual(1)
+describe('Register e2e tests', () => {
+  // Esperar o app ficar pronto
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  // Garantir que depois que os testes executarem, aguardar o app fechar
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('deve ser possível se registrar', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Fulano da Silva',
+      email: 'fulano@gmail.com',
+      password: '12345566',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+
+  it('se tentar cadastrar um usuário com o mesmo e-mail, devolve conflito', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Fulano da Silva',
+      email: 'fulano@gmail.com',
+      password: '12345566',
+    })
+
+    const reponseWithUserAlreadyExists = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'Fulano da Silva',
+        email: 'fulano@gmail.com',
+        password: '12345566',
+      })
+
+    expect(reponseWithUserAlreadyExists.statusCode).toEqual(409)
+  })
 })

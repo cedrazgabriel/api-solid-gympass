de02ef3da11e8d71c4beaa6d22855b4f1c9bb7d9
Gabriel Cedraz <cedrazdev@gmail.com>
2024-03-02 11:18:35 -0300

    feat: implement jwt authentication

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,5 +1,10 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  // Verificar o token JWT e adicionar na variável request.user
+  await request.jwtVerify()
+
+  console.log(request.user.sub)
+
   return reply.status(200).send()
 }
